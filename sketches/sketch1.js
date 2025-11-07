15762f850896dc33bd91fea2bacbddc2ec0fa133
Jesús García Ruiz <145538559+jesusgaruiz@users.noreply.github.com>
2025-11-07 15:50:20 +0100

    Add initial sketch1.js with setup and draw functions

---
+++ sketches/sketch1.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(300, 240);
+    noLoop();
+  }
+  
+  function draw() {
+    background(255, 200, 0);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("Sketch 1", width / 2, height / 2);
+  }
+  
